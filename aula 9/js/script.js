cb3afb478b0fb05f4e1ec28e1c41832f0e39f20c
lala904 <personaestudos@gmail.com>
2025-10-08 16:15:38 -0300

    Add files via upload

---
+++ aula 9/js/script.js
@@ -0,0 +1,84 @@
+console.log("Meu primeiro JavaScript!");
+
+console.log("Meu nome é Lara \nMeu sobrenome é Andrade");
+
+console.log(`Lista de compras:
+    Arroz,
+    Feijão,
+    Açucar,
+    Café,
+    Leite`);
+
+console.log(` Lista de Estudos:
+    Matemática,
+    Português,
+    Ciências,
+    História,
+    Geografia`);
+
+console.log(`Passo a passo de fazer café:
+    1 passo: Ir até a cozinha,
+    2 passo: Ir até o armário,
+    3 passo: Abrir a porta do armário,
+    4 passo: Pegar a chaleira,
+    5 passo: Fechar a porta o armário,
+    6 passo: Ir até a pia,
+    7 passo: Abrir a chaleira,
+    8 passo: Colocar embaixo da torneira com a boca para cima da chaleira,
+    9 passo: Ligar a torneira,
+    10 passo: Encher até a quantidadde necessária,
+    11 passo: Desligar a torneira,
+    12 passo: Tampar a chaleira,
+    13 passo: Ir até o fogão,
+    14 passo: Colocar na boca do fogão,
+    15 passo: Colocar a chaleira encima da grade com a chama ligada,
+    16 passo: Esperar a água fever,
+    17 passo: Ir até ob armário,
+    18 passo: Pegar o bule, a colher, o coador, o açucar e o pó de café,
+    19 passo: Ir até o balcão,
+    20 passo: Colocar o coador no bule e dentro do coador coolocar café,
+    21 passo: Ir até o fogão,
+    22 passo: Desligar o fogo,
+    23 passo: Pegar um pano de prato e segurar a alça de chaleira e segurar
+    24 passo: Viar a água quente no coador,
+    25 passo: Esperar filtrar o café,
+    26 passo: Colocar a chaleira na pia,
+    27 passo: Pegar a garrafa de café no balcão,
+    28 passo: Despejar o café que está no bule na garrafa,
+    29 passo: Fechar a garrafa`)
+
+console.log(`Passo a passo de abrir a janela:
+    1 passo: Levantar da cama,
+    2 passo: Abrir a cortina do lado esquerdo,
+    3 passo: Abrir a cortina do lado direito,
+    4 passo: Abrir a Janela do lado esquerdo,
+    5 passo: Abrir a Janela do lado direito`)
+
+console.log(`Passo a passo de ir embora:
+    0 passo: Desligar o computador,
+    1 passo: Levantar da cadeira,
+    2 passo: Sair da sala,
+    3 passo: Descer para a portaria,
+    4 passo: Descer a rua e virar a direita,
+    5 passo: Subir na passarela e descer da passarela,
+    6 passo: Entrar na cabine do move,
+    7 passo: Esperar o ônibus dentro da cabine ,
+    8 passo: Sair da cabine e entrar no ônibus,
+    9 passo: Descer perto shooping estação,
+    10 passo: Subir a passarela e descer dentro da estação metro/shooping,
+    11 passo: Atravessar a estação do metro e virar a esquerda,
+    12 passo: Seguir reto e virar a direita e seguir reto novamente,
+    13 passo: Sair da estação de metro/shooping e virar a direita em direção do bairro Vila Clóris,
+    14 passo: Seguir reto e subir o morro,
+    15 passo: Terminar de subir o morro,
+    16 passo: Virar a direita,
+    17 passo: Atravessar a rua enquanto olha pelo lado esquerdo,
+    18 passo: Terminar de atravessar,
+    19 passo: Atravessar novamente e agora olha pelo lado direito,
+    20 passo: Terminar de atravessar,
+    21 passo: Ir até a porta da minha casa,
+    22 passo: Abrir a mochila e pegar a chave,
+    23 passo: Abrir a porta,
+    24 passo: Entrar na casa,
+    25 passo: Fechar a porta,
+    26 passo: Guardar a chave`)
